77ef2cd6d2ac6f8008dbfbed76708ab8064fbeb0
Keith Ng <keith@point.org.nz>
2018-02-12 14:46:13 +1300

    Added bundletest for building and analysing bundles.

---
+++ webpack.test.js
@@ -0,0 +1,14 @@
+const webpack  = require('webpack')
+const merge    = require('webpack-merge')
+const common   = require('./webpack.common.js')
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+
+module.exports = merge(common, {
+  plugins: [
+    new UglifyJsPlugin(),
+    new BundleAnalyzerPlugin()
+  ]
+})
