21dfd887326026ec69a9acf84207f1650743f4c7
Keith Ng <keith@point.org.nz>
2018-05-10 17:26:58 +1200

    Stripped down loading page

--- src/loading.js
+++ src/loading.js
@@ -1,18 +1,4 @@
-import $ from "jquery"
-
 import "./loading.less"
 const HTML = require("./loading.html")
-
-
-class Loading {
-  constructor () {
-    this.$ = $(HTML)
-    this.$.appendTo("body")
-  }
-
-  fadeOut () {
-    this.$.fadeTo(600, 0.01, () => this.$.remove())
-  }
-}
-
-export default new Loading()
+let doc = new DOMParser().parseFromString(HTML, "text/html")
+document.body.appendChild(doc.firstChild)
