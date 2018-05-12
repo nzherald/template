5c08c70b70c0cb94ce3ee1dd2beba3d48e39c7a2
Keith Ng <keith@point.org.nz>
2018-05-12 15:44:31 +1200

    Included template for JS modules.

--- src/root.js
+++ src/root.js
@@ -5,13 +5,16 @@ import * as d3jetpack from "d3-jetpack"
 
 import "./base.less"
 import "./root.less"
+import Template from "./lib/module-template.js"
 const HTML = require("./root.html")
 
 
 class Main {
     constructor () {
         $(HTML).appendTo("#root")
-        $("<div>", { html : "Javascript goes here." }).appendTo("#root")
+		const T = new Template({
+			container: "#root"
+		})
         $("#loading").fadeTo(600, 0.01, () => $("#loading").remove())
     }
 }
