8b7d55d3dc28d98d6fb75cc8d6fa80732521acb4
Keith Ng <keith@point.org.nz>
2018-05-21 15:43:26 +1200

    thick-dev for testing inside a nzherald.co.nz shell

---
+++ webpack.thick-dev.js
@@ -0,0 +1,9 @@
+const merge = require("webpack-merge")
+const base = require("./webpack.dev.js")
+
+// Spins up dev server with bundles using nzherald.co.nz template
+module.exports = merge(base, {
+    devServer: {
+        contentBase: "./static/thick-dev"
+    }
+})
