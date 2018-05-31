db6e344acb90906c8b9f8114c727fea3175cec7a
Keith Ng <keith@point.org.nz>
2018-05-31 23:53:21 +1200

    root.js update

--- src/root.js
+++ src/root.js
@@ -1,18 +1,35 @@
-import "./base.less"
-import "./root.less"
+import _ from "lodash"
+import $ from "jquery"
+import * as d3 from "d3"
+import * as d3jetpack from "d3-jetpack"
+
 import Template from "./lib/module-template.js"
 import HTML from "./root.html"
+import "./base.less"
+import "./root.less"
 
 
 class Main {
     constructor () {
         $(HTML).appendTo("#root")
         const T = new Template({
-            container: "#root"
+            container: "#root .mainvis"
         })
+        this.fadeOut()
+    }
+
+    cleanData (rawData) {
+        console.log("Raw data:", rawData)
+        return _.map(rawData, d => {
+            return d
+        })
+    }
+
+    fadeOut (b) {
         $("#loading").fadeTo(600, 0.01, () => {
             $("#loading").remove()
             console.log("Loader removed.")
+            if (b) b()
         })
     }
 }
