0adcda940e6f5462a0dc1050ee261e52413b849c
Keith Ng <keith@point.org.nz>
2018-06-05 15:31:59 +1200

    Merged dev-thick back into dev
    Created thick.html and iframe.html instead (to be used on dev server)

--- webpack.dev-thick.js
+++
@@ -1,9 +0,0 @@
-const merge = require("webpack-merge")
-const base = require("./webpack.dev.js")
-
-// Spins up dev server with bundles using nzherald.co.nz template
-module.exports = merge(base, {
-    devServer: {
-        contentBase: "./static-thick"
-    }
-})
