963fccd4f23c22b7416a1ba1c9ab497f30976809
Keith Ng <keith@point.org.nz>
2019-04-30 12:49:59 +1200

    Add dev files to deploy

--- webpack.deploy.js
+++ webpack.deploy.js
@@ -3,6 +3,7 @@ const base = require("./webpack.prod.js")
 const EmbedPlugin = require("./util/embed.js")
 const {smallUploader, largeUploader} = require("./util/uploader")
 const package = require("./package.json")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 // Generates embed.js and deploys to s3.newsapps.nz/dev/[package name]/
 const host = "http://s3.newsapps.nz",
@@ -11,6 +12,7 @@ module.exports = merge(base, {
     plugins: [
         new EmbedPlugin({basePath: host + path}),
         largeUploader({basePath: path}),
-        smallUploader({basePath: path})
+        smallUploader({basePath: path}),
+        new CopyWebpackPlugin(["static-dev"])
     ]
 })
