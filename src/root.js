a3d30aaf2150f4a02c93e7e67a5d226c96205791
Keith Ng <keith@point.org.nz>
2019-05-01 13:19:47 +1200

    Batteries included always has jQuery

--- src/root.js
+++ src/root.js
@@ -69,20 +69,11 @@ class Main {
 
     fadeOut (b) {
         sessionStorage.setItem("loading", "done");
-        if (typeof($) !== "undefined") {
-            $("#loading").fadeTo(600, 0.01, () => {
-                $("#loading").remove()
-                console.log("Loading screen removed.")
-                if (b) b()
-            })
-        } else {
-            var loadingRemove = document.getElementById("loading")
-            if (loadingRemove) {
-                document.body.removeChild(loadingRemove)
-                console.log("Loading screen removed.")
-            }
+        $("#loading").fadeTo(600, 0.01, () => {
+            $("#loading").remove()
+            console.log("Loading screen removed.")
             if (b) b()
-        }
+        })
     }
 }
 
