835de096ad6401a667d10cf2e066accf5cbf0173
Chris Knox <chris.knox@nzherald.co.nz>
2019-08-09 14:19:52 +1200

    be a bit more robust with the CORS fix

--- src/lib/base.js
+++ src/lib/base.js
@@ -11,7 +11,7 @@ class Base {
             $: $(root)
         }
         this.root.$.html("").append(html)
-        this.basePath = ENV.basePath.includes(location.host) ? ENV.basePath.replace("apps", "apps-mirror") : ENV.basePath
+        this.basePath = (ENV.production && ENV.separateCrossOriginRequests && ENV.basePath.includes(location.host)) ? ENV.localPath : ENV.basePath
     }
 
     premiumWait (render) {
