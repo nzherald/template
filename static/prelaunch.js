9043fd0c160ba2a7559f5fc746622245957bea0d
Chris Knox <chris.knox@nzherald.co.nz>
2020-01-16 10:17:02 +1300

    not a rating tool any more

--- static/prelaunch.js
+++ static/prelaunch.js
@@ -3,7 +3,7 @@
     var isIE = navigator.appName == 'Microsoft Internet Explorer' || !!(navigator.userAgent.match(/Trident/)||navigator.userAgent.match(/rv:11/));
     if (isIE) {
         if (el) {
-            el.innerHTML = '<b>Sorry! Your browser does not support the rating tool.</b> <p>Please try <a href="https://www.microsoft.com/en-nz/windows/microsoft-edge" target="_blank">Microsoft Edge</a>, <a href="https://www.google.com/chrome/" _target="_blank">Google Chrome</a> or <a href="https://www.mozilla.org/en-US/firefox/new/" _target="_blank">Mozilla Firefox</a>.</p>'
+            el.innerHTML = '<b>Sorry! Your browser does not support this interactive.</b> <p>Please try <a href="https://www.microsoft.com/en-nz/windows/microsoft-edge" target="_blank">Microsoft Edge</a>, <a href="https://www.google.com/chrome/" _target="_blank">Google Chrome</a> or <a href="https://www.mozilla.org/en-US/firefox/new/" _target="_blank">Mozilla Firefox</a>.</p>'
         }
         return;
     };
