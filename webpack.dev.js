7a186fffc3c76727dd86f80b609025ae2d240229
Chris Knox <chris.knox@nzherald.co.nz>
2020-05-06 12:17:18 +1200

    enable elm debugger

--- webpack.dev.js
+++ webpack.dev.js
@@ -33,7 +33,11 @@ module.exports = merge(base, {
                 exclude: [/elm-stuff/, /node_modules/],
                 use: [
                     { loader: 'elm-hot-webpack-loader' },
-                    { loader: 'elm-webpack-loader' }
+                    { loader: 'elm-webpack-loader', 
+                        options: {verbose: true,
+                            debug: true
+                        }
+                    }
                 ]
             }
         ]
