47e8660c21f06df6769187928d60fbff7c682bb1
Chris Knox <chris.knox@nzherald.co.nz>
2020-06-05 14:40:41 +1200

    Added scrolly capability and an automatic app warning - as options

--- src/root.js
+++ src/root.js
@@ -1,20 +1,27 @@
 import ENV from "Environment"
 import Base from "./lib/base.js"
+import { nzhconsole, setupScrolly, appWarn } from "./lib/util.js"
 import HTML from "./root.html"
+import HTML2 from "./root2.html"
 import "./root.less"
-
 
 class Main extends Base {
     constructor (selector, params) {
-        console.log("Setting up visualisation with parameters:", params)
-        super(selector, HTML)
-        console.log("Loading data...")
+        nzhconsole("Setting up visualisation with parameters:", params)
+        super(selector, params.chart == "māori" ? HTML2 : HTML)
+        nzhconsole("Loading data...")
         null
         this.premiumWait(() => {
-            console.log("Rendering...")
+            nzhconsole("Rendering...")
             null
-            console.log("Done.")
+            nzhconsole("Done.")
             this.fadeOut()
+            if (params.setupScrolly) {
+                const scrolly = setupScrolly(params.setupScrolly)
+            }
+            if (params.appWarn) {
+                appWarn(this.root.selector, params.appWarn, params.category)
+            }
         })
     }
 }
