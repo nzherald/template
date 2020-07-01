ae4948f696d6af06bffb8189f6c0748142e4af4a
Chris Knox <chris.knox@nzherald.co.nz>
2020-07-01 17:23:47 +1200

    moved simple scrolly in a branch called simple-scrolly

--- src/root.js
+++ src/root.js
@@ -1,24 +1,20 @@
 import ENV from "Environment"
 import Base from "./lib/base.js"
-import { nzhconsole, setupScrolly, appWarn } from "./lib/util.js"
+import { appWarn } from "./lib/util.js"
 import HTML from "./root.html"
-import HTML2 from "./root2.html"
 import "./root.less"
 
 class Main extends Base {
     constructor (selector, params) {
-        nzhconsole("Setting up visualisation with parameters:", params)
-        super(selector, params.chart == "māori" ? HTML2 : HTML)
-        nzhconsole("Loading data...")
+        console.log("Setting up visualisation with parameters:", params)
+        super(selector, HTML)
+        console.log("Loading data...")
         null
         this.premiumWait(() => {
-            nzhconsole("Rendering...")
+            console.log("Rendering...")
             null
-            nzhconsole("Done.")
-            this.fadeOut()
-            if (params.setupScrolly) {
-                const scrolly = setupScrolly(params.setupScrolly)
-            }
+            console.log("Done.")
+            //this.fadeOut()
             if (params.appWarn) {
                 appWarn(this.root.selector, params.appWarn, params.category)
             }
