91bede84cee7254aaea9f04b7e2507c8564a8eb8
Keith Ng <keith@point.org.nz>
2020-10-31 18:31:44 +1300

    I was changing the fusion data used to build the page when I should've been changing the page. :-( :-( :-(

--- util/rip-test.js
+++ util/rip-test.js
@@ -8,27 +8,42 @@ function escapeChunk (chunk) {
                 .replace(/"/g, "\\\"") // Escape quotes
 }
 
-function replaceFooter (indexFn, footerFn) {
+// function replaceFusionFooter (indexFn, footerFn) {
+//     console.log("Replacing fusion footer data in index.html...")
+//     let success = false
+//     const $ = cheerio.load(fs.readFileSync(indexFn, "utf-8"))
+//     const footer = fs.readFileSync(footerFn, "utf-8")
+//     const orig = new RegExp(/raw_text_footer","content":".+?"}/)
+//     const targ = `raw_text_footer","content":"${escapeChunk(footer)}"}`
+//     $("script").each((i, e) => {
+//         const chunk = $(e).html()
+//         if (chunk.match(orig)) {
+//             $(e).text(chunk.replace(orig, targ)) // Use text not HTML, as HTML will cut all the escape characters
+//             success = true
+//             return false
+//         }
+//     })
+//     if (success) {
+//         console.log("Success!")
+//         fs.writeFileSync(indexFn, $.html(), "utf8")
+//     }
+//     else {
+//         console.error("Could not find embed footer chunk (hint: search for \"raw_text_footer\" in index.html)!")
+//     }
+// }
+
+function replaceLiveFooter (indexFn, footerFn) {
     console.log("Replacing embed footer in index.html...")
     let success = false
     const $ = cheerio.load(fs.readFileSync(indexFn, "utf-8"))
     const footer = fs.readFileSync(footerFn, "utf-8")
-    const orig = new RegExp(/raw_text_footer","content":".+?"}/)
-    const targ = `raw_text_footer","content":"${escapeChunk(footer)}"}`
-    $("script").each((i, e) => {
-        const chunk = $(e).html()
-        if (chunk.match(orig)) {
-            $(e).text(chunk.replace(orig, targ)) // Use text not HTML, as HTML will cut all the escape characters
-            success = true
-            return false
-        }
-    })
-    if (success) {
+    if ($(".article__raw-html__bottom")) {
+        $(".article__raw-html__bottom").html(footer)
         console.log("Success!")
         fs.writeFileSync(indexFn, $.html(), "utf8")
     }
     else {
-        console.error("Could not find embed footer chunk (hint: search for \"raw_text_footer\" in index.html)!")
+        console.error("Could not find embed footer chunk (hint: search for \"article__raw-html__bottom\" in index.html)!")
     }
 }
 
@@ -66,5 +81,5 @@ function hackAntiPiracy (indexFn, defaultjsFn, rubberStampFn) {
     }
 }
 
-replaceFooter(`${process.argv[2]}/index.html`, `${process.argv[3]}/footer.html`)
+replaceLiveFooter(`${process.argv[2]}/index.html`, `${process.argv[3]}/footer.html`)
 hackAntiPiracy(`${process.argv[2]}/index.html`, `${process.argv[2]}/default.js`, `${process.argv[3]}/rubberstamp.html`)
