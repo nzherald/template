2f2fab1e51152798d3237c671ac9a4cbc3fa3192
Keith Ng <keith@point.org.nz>
2020-11-02 16:08:02 +1300

    Oops

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const base = require("./webpack.common.js")
 const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const autoprefixer = require("autoprefixer")
-const { homepage } = require("./package.json")
+const { homepage, name } = require("./package.json")
 
 // Post-processing and minification of bundle
 module.exports = merge(base, {
