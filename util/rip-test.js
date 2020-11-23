2cefc227403fd05d4d1b9c77b9f97fa143c55dea
Keith Ng <keith@point.org.nz>
2020-11-23 19:48:05 +1300

    Remove cruft from rip-test. Fusion data for footer is NOT actually being used to generate the footer at run-time, so just manipulate the generated footer and ignore the (very difficult to work with) data.

--- util/rip-test.js
+++ util/rip-test.js
@@ -2,40 +2,8 @@ const fs = require('fs')
 const cheerio = require('cheerio')
 const { makeFooter } = require('./embedgen.js')
 
-// The footer is a chunk of HTML in a string in an object in a script tag (euuuggghhh)
-function escapeChunk (chunk) {
-    return chunk.replace(/\n\s*/g, "") // Remove newlines
-                .replace(/\//g, "\\\/") // Escape slashes
-                .replace(/"/g, "\\\"") // Escape quotes
-}
-
-// function replaceFusionFooter (indexFn, footerFn) {
-//     console.log("Replacing fusion footer data in index.html...")
-//     let success = false
-//     const $ = cheerio.load(fs.readFileSync(indexFn, "utf-8"))
-//     const footer = fs.readFileSync(footerFn, "utf-8")
-//     const orig = new RegExp(/raw_text_footer","content":".+?"}/)
-//     const targ = `raw_text_footer","content":"${escapeChunk(footer)}"}`
-//     $("script").each((i, e) => {
-//         const chunk = $(e).html()
-//         if (chunk.match(orig)) {
-//             $(e).text(chunk.replace(orig, targ)) // Use text not HTML, as HTML will cut all the escape characters
-//             success = true
-//             return false
-//         }
-//     })
-//     if (success) {
-//         console.log("Success!")
-//         fs.writeFileSync(indexFn, $.html(), "utf8")
-//     }
-//     else {
-//         console.error("Could not find embed footer chunk (hint: search for \"raw_text_footer\" in index.html)!")
-//     }
-// }
-
 function replaceLiveFooter (indexFn) {
     console.log("Replacing embed footer in index.html...")
-    let success = false
     const $ = cheerio.load(fs.readFileSync(indexFn, "utf-8"))
     const footer = makeFooter("#nzh-datavis-root", "./", "DataVisDevMain", "Local testing footer running!")
     if ($(".article__raw-html__bottom")) {
